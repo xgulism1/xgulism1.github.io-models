ad4dd14bdcb3a9be41a6f1953f951ed1d69c71e5
xgulism1 <gulis.martin@gmail.com>
2021-05-02 20:17:52 +0200

    Update assign-model.js

--- assign-model.js
+++ assign-model.js
@@ -1,13 +1,23 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-if (urlParams.has('model')) {
-  const filename = urlParams.get('filename')
-  const friendlyName = urlParams.get('friendly-name')
+const modelViewer = document.querySelector('model-viewer');
+
+if (urlParams.has('filename')) {
+  const filename = urlParams.get('filename');
+
+  if (modelViewer != null) {
+    modelViewer.setAttribute('src', `models-android/${filename}.glb`);
+    modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
+    modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
+  }
+}
+
+if (urlParams.has('friendly-name')) {
+  const friendlyName = urlParams.get('friendly-name');
   
-  const modelViewer = document.querySelector('model-viewer');
-  modelViewer.setAttribute('src', `models-android/${filename}.glb`);
-  modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
-  modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
-  modelViewer.setAttribute('alt', `${friendlyName}`);
+  if (modelViewer != null) {
+    modelViewer.setAttribute('alt', `${friendlyName}`);
+  }
+  
   document.title = `${friendlyName}`;
 }
