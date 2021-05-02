fda74e0a9d61abdc8f2d9ebec306d65637e6f672
xgulism1 <gulis.martin@gmail.com>
2021-05-02 18:18:25 +0200

    Update ios-redirect.js

--- pages/ios-redirect.js
+++ pages/ios-redirect.js
@@ -1,25 +1,16 @@
-console.log("test123");
-
-window.addEventListener('load', function () {
-  console.log('This function is executed once the page is fully loaded');
-});
-
-(() => {
-  console.log('This function runs now');
-})();
+// @see https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 
 // Test if this is iOS
-var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-console.log(isIOS);
+//var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+//console.log(isIOS);
 //if (isIOS) {
   // Use interval to check if model-viewer exists and try to start the iOS AR Quick Look immediatelly
   var checkExist = setInterval(function() {
     modelViewer = document.querySelector("model-viewer");
     if (modelViewer != null) {
       // Start the iOS AR Quick Look
-      console.log('activateAR');
       modelViewer.activateAR();
-      //clearInterval(checkExist);
+      clearInterval(checkExist);
     }
-  }, 5000);
+  }, 500);
 //}
