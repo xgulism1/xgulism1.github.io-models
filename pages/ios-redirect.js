18fa7f306d1e9c21feda7c5abf5fd4f20ea784d5
xgulism1 <gulis.martin@gmail.com>
2021-05-02 17:03:23 +0200

    Update ios-redirect.js

--- pages/ios-redirect.js
+++ pages/ios-redirect.js
@@ -1,4 +1,13 @@
-var isMobile = /iPhone|iPad|iPod|i.test(navigator.userAgent);
-if (isMobile) {
-  window.location.href = "../models-ios/eMotor_Explode.usdz"
+/* Test if this is iOS */
+var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+if (isIOS) {
+  /* Use interval to check if model-viewer exists and try to start the iOS AR Quick Look immediatelly */
+  var checkExist = setInterval(function() {
+    modelViewer = document.querySelector("model-viewer");
+    if (modelViewer != null) {
+      /* Start the iOS AR Quick Look */
+      modelViewer.activateAR();
+      clearInterval(checkExist);
+    }
+  }, 500);
 }
