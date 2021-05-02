47f090c12000ff20b8980b8cdda0ebf4687b831f
xgulism1 <gulis.martin@gmail.com>
2021-05-02 22:21:16 +0200

    Update assign-model.js

--- assign-model.js
+++ assign-model.js
@@ -1,32 +1,27 @@
-//var checkExist = setInterval(function() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
+// Get URL params
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// Assign title
+if (urlParams.has('friendly-name')) {
+  const friendlyName = urlParams.get('friendly-name');
+  document.title = `${friendlyName}`;
+}
+
+// Try to assign model to the model-viewer
+var checkExist = setInterval(function() {
   const modelViewer = document.querySelector('model-viewer');
-console.log(modelViewer);
-
-  if (urlParams.has('filename')) {
-    const filename = urlParams.get('filename');
-
-    if (modelViewer != null) {
+  if (modelViewer) {
+    if (urlParams.has('filename')) {
+      const filename = urlParams.get('filename');
       modelViewer.setAttribute('src', `models-android/${filename}.glb`);
       modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
       modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
     }
-  }
-
-  if (urlParams.has('friendly-name')) {
-    const friendlyName = urlParams.get('friendly-name');
-
-    if (modelViewer != null) {
+    if (urlParams.has('friendly-name')) {
+      const friendlyName = urlParams.get('friendly-name');
       modelViewer.setAttribute('alt', `${friendlyName}`);
     }
-
-    document.title = `${friendlyName}`;
-  }
-  
-  /*if (modelViewer != null) {
-    console.log("exists")
     clearInterval(checkExist);
-  }*/
-  
-//}, 500);
+  }
+}, 500);
