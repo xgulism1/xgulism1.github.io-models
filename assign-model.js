1c8bfec5d3fc333eed56555021584d07415f5796
xgulism1 <gulis.martin@gmail.com>
2021-05-02 20:06:49 +0200

    Create assign-model.js

---
+++ assign-model.js
@@ -0,0 +1,12 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.has('model')) {
+  const modelName = urlParams.get('model-name')
+  const modelAlt = urlParams.get('model-alt')
+  
+  const modelViewer = document.querySelector('model-viewer');
+  modelViewer.setAttribute('src', `../models-android/${modelName}.glb`);
+  modelViewer.setAttribute('ios-src', `../models-ios/${modelName}.usdz`);
+  modelViewer.setAttribute('poster', `../thumbnails/${modelName}.jpg`);
+  modelViewer.setAttribute('alt', `${modelAlt}`);
+}
