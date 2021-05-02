bfc3814df111f7352fff018d20d6e77cbcbd3576
xgulism1 <gulis.martin@gmail.com>
2021-05-02 20:22:18 +0200

    Update assign-model.js

--- assign-model.js
+++ assign-model.js
@@ -1,23 +1,30 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const modelViewer = document.querySelector('model-viewer');
+var checkExist = setInterval(function() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const modelViewer = document.querySelector('model-viewer');
 
-if (urlParams.has('filename')) {
-  const filename = urlParams.get('filename');
+  if (urlParams.has('filename')) {
+    const filename = urlParams.get('filename');
 
-  if (modelViewer != null) {
-    modelViewer.setAttribute('src', `models-android/${filename}.glb`);
-    modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
-    modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
+    if (modelViewer != null) {
+      modelViewer.setAttribute('src', `models-android/${filename}.glb`);
+      modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
+      modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
+    }
   }
-}
 
-if (urlParams.has('friendly-name')) {
-  const friendlyName = urlParams.get('friendly-name');
+  if (urlParams.has('friendly-name')) {
+    const friendlyName = urlParams.get('friendly-name');
+
+    if (modelViewer != null) {
+      modelViewer.setAttribute('alt', `${friendlyName}`);
+    }
+
+    document.title = `${friendlyName}`;
+  }
   
   if (modelViewer != null) {
-    modelViewer.setAttribute('alt', `${friendlyName}`);
+      clearInterval(checkExist);
   }
   
-  document.title = `${friendlyName}`;
-}
+}, 500);
