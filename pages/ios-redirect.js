560d61006ad173dbf47e288dc33fb926b2771f55
xgulism1 <gulis.martin@gmail.com>
2021-05-02 17:30:46 +0200

    Update ios-redirect.js

--- pages/ios-redirect.js
+++ pages/ios-redirect.js
@@ -8,18 +8,16 @@ window.addEventListener('load', function () {
   console.log('This function runs now');
 })();
 
-(() => {
-  // Test if this is iOS
-  var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-  if (isIOS) {
-    // Use interval to check if model-viewer exists and try to start the iOS AR Quick Look immediatelly
-    var checkExist = setInterval(function() {
-      modelViewer = document.querySelector("model-viewer");
-      if (modelViewer != null) {
-        // Start the iOS AR Quick Look
-        modelViewer.activateAR();
-        clearInterval(checkExist);
-      }
-    }, 500);
-  }
-})();
+// Test if this is iOS
+var isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+if (isIOS) {
+  // Use interval to check if model-viewer exists and try to start the iOS AR Quick Look immediatelly
+  var checkExist = setInterval(function() {
+    modelViewer = document.querySelector("model-viewer");
+    if (modelViewer != null) {
+      // Start the iOS AR Quick Look
+      modelViewer.activateAR();
+      clearInterval(checkExist);
+    }
+  }, 5000);
+}
