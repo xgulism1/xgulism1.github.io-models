369c646c114d2f7e7b9406bb3b9a2a5f4de59b5b
xgulism1 <gulis.martin@gmail.com>
2021-05-02 19:55:56 +0200

    Add files via upload

---
+++ ios-redirect.js
@@ -0,0 +1,27 @@
+// Test if this is iOS
+// @see https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+function isIOS() {
+  return [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform)
+  // iPad on iOS 13 detection
+  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+// Try to start the iOS AR Quick Look immediatelly
+// @see https://github.com/google/model-viewer/issues/697
+// @see https://infinitygames-kids-galicnik.glitch.me/
+if (isIOS) {
+  var checkExist = setInterval(function() {
+    modelViewer = document.querySelector("model-viewer");
+    if (modelViewer != null) {
+      modelViewer.activateAR();
+      clearInterval(checkExist);
+    }
+  }, 500);
+}
