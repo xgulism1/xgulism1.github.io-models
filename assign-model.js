f0a66f33f9bf4b215869396dffa51ac559d2bc5a
xgulism1 <gulis.martin@gmail.com>
2021-05-02 20:13:17 +0200

    Update assign-model.js

--- assign-model.js
+++ assign-model.js
@@ -1,13 +1,13 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 if (urlParams.has('model')) {
-  const modelName = urlParams.get('model-name')
-  const modelAlt = urlParams.get('model-alt')
+  const filename = urlParams.get('filename')
+  const friendlyName = urlParams.get('friendly-name')
   
   const modelViewer = document.querySelector('model-viewer');
-  modelViewer.setAttribute('src', `models-android/${modelName}.glb`);
-  modelViewer.setAttribute('ios-src', `models-ios/${modelName}.usdz`);
-  modelViewer.setAttribute('poster', `thumbnails/${modelName}.jpg`);
-  modelViewer.setAttribute('alt', `${modelAlt}`);
-  document.title = `${modelAlt}`;
+  modelViewer.setAttribute('src', `models-android/${filename}.glb`);
+  modelViewer.setAttribute('ios-src', `models-ios/${filename}.usdz`);
+  modelViewer.setAttribute('poster', `thumbnails/${filename}.jpg`);
+  modelViewer.setAttribute('alt', `${friendlyName}`);
+  document.title = `${friendlyName}`;
 }
